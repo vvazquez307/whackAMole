756bb65b4cc54611376ef46d7bc345b9954611ea
victor <vvazquez307@gmail.com>
2023-02-03 12:17:08 -0600

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 let score = 0;
+
+const holes = document.getElementsByClassName("hole");
+console.log("holes")
+setInterval(function() {
+    const randomHoleIndex = Math.floor(Math.random() * holes.length);
+    holes[randomHoleIndex].classList.toggle("mole");
+}, 250);
+
+const number = document.getElementById('score')
+
+const gameArea = document.getElementById("whack-a-mole");
+gameArea.addEventListener('click', function(clickEvent) {
+    if (clickEvent.target.matches(' .mole')){
+        clickEvent.target.classList.remove('mole');
+        number.innerText++
+    }
+});
